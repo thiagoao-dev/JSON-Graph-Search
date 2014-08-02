5af4d8b451ec8ace29c21f0e0d3e9ce4336d956a
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2014-08-02 00:54:48 -0300

    Alteração do código de leitura do arquivo json

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ __author__ = 'ThiagoAugustus'
 
 import json
 
+# Lê o arquivo json
 def readJson():
-
+    # Abre o arquivo conforme o caminho informado
     with open(input("Informe o nome do arquivo.json: ")) as json_file:
         json_file = json.load(json_file)
+        # Testa o objeto
         assert isinstance(json_file, object)
         return json_file
 
-print(readJson())
+# Recupera os vértices do json
+vertices = readJson()['vertices']
+
+def buscaEmLargura(g, no):
+    pass
