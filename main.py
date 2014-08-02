6d3a847c089e7e2defdef5c3cbe4c4b5bd27ef6b
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2014-08-02 00:22:46 -0300

    Arquivo para teste  Enunciado do trabalhno  Arquivo teste com leitura

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 __author__ = 'ThiagoAugustus'
+
+import json
+
+def readJson():
+
+    with open(input("Informe o nome do arquivo.json: ")) as json_file:
+        json_file = json.load(json_file)
+        assert isinstance(json_file, object)
+        return json_file
+
+print(readJson())
