4084b15023522038754c8ab2a8cb0e6809858d99
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2014-08-02 12:28:50 -0300

    alteração na função busca em profundidade

--- main.py
+++ main.py
@@ -18,22 +18,29 @@ def fbs(g, no):
 # Busca em Profundidade (Depth-First Search)
 def dfs(g, no, path = []):
     if no in g.keys():
-        if no not in path:
-            print("Expandido nó : ", no)
-            path.append(no)
-            '''
-            if str(g[no][0]) not in path:
-                print("Expadir nó : " + str(g[no][0]))
-                dfs(g,str(g[no][0]),path)
-            elif str(g[no][1]) not in path:
-                print("Expadir nó : " + str(g[no][1]))
-                dfs(g,str(g[no][1]),path)
-            '''
+        if no not in path: return False
+        print("Expandido nó : ", no)
+        path.append(no)
+        if str(g[no][0]) not in path:
+            print("Expadir nó : " + str(g[no][0]))
+            findNextNode(g,g[no][0])
+            #dfs(g,str(g[no][0]),path)
+        elif str(g[no][1]) not in path:
+            print("Expadir nó : " + str(g[no][1]))
+            findNextNode(g,g[no][1])
+            #dfs(g,str(g[no][1]),path)
     else:
+        print("Saída : ", path)
         return False
 
 def findNextNode(g, no):
-    pass
+    nodes = []
+    print(g, no)
+    nodes = g[str(no)]
+    for nos, val in g.items():
+        if no == val[0] or no == val[1]:
+            print(nos, " entrou ", val)
+    print(nodes)
 
 # Recupera os vértices do json
 arquivo  = readJson()
