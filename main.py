687bceee4018e741cf7840939ca499d6cb35f722
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2014-08-02 10:59:38 -0300

    Criação das funções busca em largura e busca em profundidade (ainda não implementados)

--- main.py
+++ main.py
@@ -11,8 +11,31 @@ def readJson():
         assert isinstance(json_file, object)
         return json_file
 
-# Recupera os vértices do json
-vertices = readJson()['vertices']
-
-def buscaEmLargura(g, no):
+# Busca em Largura (Breadth-First Search
+def fbs(g, no):
     pass
+
+# Busca em Profundidade (Depth-First Search)
+def dfs(g, no, path = []):
+    if no in g.keys():
+        if no not in path:
+            print("Expandido nó : ", no)
+            path.append(no)
+            '''
+            if str(g[no][0]) not in path:
+                print("Expadir nó : " + str(g[no][0]))
+                dfs(g,str(g[no][0]),path)
+            elif str(g[no][1]) not in path:
+                print("Expadir nó : " + str(g[no][1]))
+                dfs(g,str(g[no][1]),path)
+            '''
+    else:
+        return False
+
+def findNextNode(g, no):
+    pass
+
+# Recupera os vértices do json
+arquivo  = readJson()
+vertices = arquivo['vertices']
+dfs(vertices,input("Qual vértice iniciará a busca: "))
